8e9e437134a179dafe2c597084725b4010a6c96b
jcw1031 <jcw001031@gmail.com>
2023-04-18 20:05:28 +0900

    Spring BindingResult 사용

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -7,12 +7,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/validation/v2/items")
@@ -48,31 +53,34 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
-        // 검증 오류 결과 보관
-        Map<String, String> errors = new HashMap<>();
+    public String addItemV1(
+            @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model
+    ) {
+        log.info("item = {}", item);
         // 검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
-            errors.put("itemName", "상품 이름은 필수입니다.");
+            bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
         }
         if (item.getPrice() == null || item.getPrice() < MIN_PRICE || item.getPrice() > MAX_PRICE) {
-            errors.put("price", "가격은 1,000원 ~ 1,000,000원 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "price",
+                    "가격은 1,000원 ~ 1,000,000원 까지 허용합니다."));
         }
         if (item.getQuantity() == null || item.getQuantity() >= MAX_QUANTITY) {
-            errors.put("quantity", "수량은 최대 9,999개 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "quantity",
+                    "수량은 최대 9,999개 까지 허용합니다."));
         }
 
         // 특정 필드가 아닌 복합 룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < MIN_TOTAL_PRICE) {
-                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice);
+                bindingResult.addError(new ObjectError("item",
+                        "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
             }
         }
 
-        if (hasError(errors)) {
-            log.error("errors = {}", errors);
-            model.addAttribute("errors", errors);
+        if (bindingResult.hasErrors()) {
+            log.error("errors = {}", bindingResult);
             return "validation/v2/addForm";
         }
 
@@ -81,10 +89,6 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
-    }
-
-    private static boolean hasError(Map<String, String> errors) {
-        return !errors.isEmpty();
     }
 
     @GetMapping("/{itemId}/edit")
