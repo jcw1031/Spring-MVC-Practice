60aa7e81f1c5dab66885e620edd1f40a85eec59d
jcw1031 <jcw001031@gmail.com>
2024-01-20 12:11:45 +0900

    커스텀 ArgumentResolver를 사용해 로그인 사용자 파라미터 전달

--- login/src/main/java/hello/login/WebConfiguration.java
+++ login/src/main/java/hello/login/WebConfiguration.java
@@ -1,15 +1,18 @@
 package hello.login;
 
+import hello.login.web.argumentresolver.LoginMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
 import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -23,7 +26,7 @@ public class WebConfiguration implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-//    @Bean
+    //    @Bean
     public FilterRegistrationBean<Filter> loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
@@ -43,5 +46,10 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 }
