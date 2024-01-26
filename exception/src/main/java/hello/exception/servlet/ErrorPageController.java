2decaf603d438e2ace152d62482f7c0402edd0aa
jcw1031 <jcw001031@gmail.com>
2024-01-26 18:45:02 +0900

    API 예외 처리

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -3,19 +3,24 @@ package hello.exception.servlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static jakarta.servlet.RequestDispatcher.ERROR_EXCEPTION;
+import static jakarta.servlet.RequestDispatcher.ERROR_EXCEPTION_TYPE;
+import static jakarta.servlet.RequestDispatcher.ERROR_MESSAGE;
+import static jakarta.servlet.RequestDispatcher.ERROR_REQUEST_URI;
+import static jakarta.servlet.RequestDispatcher.ERROR_SERVLET_NAME;
+import static jakarta.servlet.RequestDispatcher.ERROR_STATUS_CODE;
 
 @Slf4j
 @Controller
 public class ErrorPageController {
-
-    private static final String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
-    private static final String ERROR_EXCEPTION_TYPE = "jakarta.servlet.error.exception_type";
-    private static final String ERROR_MESSAGE = "jakarta.servlet.error.message";
-    private static final String ERROR_REQUEST_URI = "jakarta.servlet.error.request_uri";
-    private static final String ERROR_SERVLET_NAME = "jakarta.servlet.error.servlet_name";
-    private static final String ERROR_STATUS_CODE = "jakarta.servlet.error.status_code";
 
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
@@ -29,6 +34,22 @@ public class ErrorPageController {
         log.info("errorPage 500");
         printErrorInfo(request);
         return "error-page/500";
+    }
+
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(
+            HttpServletRequest request, HttpServletResponse response
+    ) {
+        log.info("API errorPage 500");
+        Map<String, Object> result = new HashMap<>();
+
+        Exception exception = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", exception.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(ERROR_STATUS_CODE);
+        return ResponseEntity.status(statusCode)
+                .body(result);
     }
 
     private void printErrorInfo(HttpServletRequest request) {
