e5fff3980d20021315ee04527602d1cceb08aab2
jcw1031 <jcw001031@gmail.com>
2024-03-09 14:16:26 +0900

    file info 로깅

--- upload/src/main/java/hello/upload/controller/ServletUploadControllerV2.java
+++ upload/src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -5,12 +5,16 @@ import jakarta.servlet.http.Part;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 
 @Slf4j
@@ -38,8 +42,34 @@ public class ServletUploadControllerV2 {
 
         parts.stream()
                 .peek(part -> log.info("name() = {}", part.getName()))
-                .flatMap(part -> part.getHeaderNames().stream()
-                        .peek(headerName -> log.info("header = {} : {}", headerName, part.getHeader(headerName))));
+                .forEach(part -> part.getHeaderNames().forEach(headerName ->
+                        log.info("header = {} : {}", headerName, part.getHeader(headerName))));
+
+        for (Part part : parts) {
+            String name = part.getName();
+            log.info("name = {}", name);
+            Collection<String> headerNames = part.getHeaderNames();
+            for (String headerName : headerNames) {
+                log.info("header = {} : {}", headerName, part.getHeader(headerName));
+            }
+
+            // 편의 메서드
+            // content-disposition; filename
+            log.info("submittedFileName = {}", part.getSubmittedFileName());
+            log.info("size = {}", part.getSize()); // part body size
+
+            // 데이터 읽기
+            InputStream inputStream = part.getInputStream();
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            log.info("body = {}", body);
+
+            // 파일에 저장하기
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = fileDirectory + part.getSubmittedFileName();
+                log.info("fullPath = {}", fullPath);
+                part.write(fullPath);
+            }
+        }
 
         return "upload-form";
     }
